3ff62910b65f9b6b60d2400b5a39598770f305ae
edenn <104012854+ensc1212@users.noreply.github.com>
2023-06-25 17:17:15 +0800

    Update app.py

--- streamlit/app.py
+++ streamlit/app.py
@@ -557,15 +557,15 @@ if selected == 'Consumers':
             with col1:
                 paleo_puffs = images_path+'/images/paleo_puffs.png'
                 st.image(paleo_puffs, width=400)
-                st.markdown('<p style="font-size:20px; text-align:left;">Paleo Puffs</p>', unsafe_allow_html=True)
+                st.markdown('<p style="font-size:20px; text-align:center;">Paleo Puffs</p>', unsafe_allow_html=True)
             with col2:
                 paleo_chips = images_path+'/images/paleo_chips.webp'
                 st.image(paleo_chips, width=300)
-                st.markdown('<p style="font-size:20px; text-align:left;">Paleo Chips</p>', unsafe_allow_html=True)
+                st.markdown('<p style="font-size:20px; text-align:center;">Paleo Chips</p>', unsafe_allow_html=True)
             with col3:
                 paleo_muesli = images_path+'/images/paleo_muesli.png'
                 st.image(paleo_muesli, width=250)
-                st.markdown('<p style="font-size:20px; text-align:left;">Paleo Muesli</p>', unsafe_allow_html=True)
+                st.markdown('<p style="font-size:20px; text-align:center;">Paleo Muesli</p>', unsafe_allow_html=True)
         else:
             col1, col2, col3 = st.columns(3)
             with col1:
